c0cd150486fa60a65862b5d49896583592dd2548
Luso1221 <luso1221@gmail.com>
2022-05-02 17:34:00 +0900

    Add tests (2)

--- test/2-stage-test.js
+++ test/2-stage-test.js
@@ -1,8 +1,6 @@
 var Admin = artifacts.require("./Admin.sol");
-const timeMachine = require("./helper/time-machine");
-const objectHash = require("object-hash");
 const datefns = require("date-fns");
-const ethCrypto = require('eth-crypto');
+const EthCrypto = require("eth-crypto");
 // const truffleAssert = require('truffle-assertions');
 
 // Start stage
@@ -16,11 +14,7 @@ describe('Stage test', function () {
     const client = accounts[0]; 
     const baseDeposit = 100;
     let SC;
-    let model;
       
-    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-      modulusLength: 2048,
-    });
     beforeEach('deploy contract', async function () {
         SC = await Admin.new();
     })
@@ -38,7 +32,7 @@ describe('Stage test', function () {
       }
       // Y ipfs
       // 1 = IPFS(M(0) | dtest | task desc | timestamp now | nonce )
-      let ipfsHash = objectHash(ipfsData);
+      let ipfsHash =  EthCrypto.hash.keccak256(ipfsData);
       console.log(ipfsData);
       console.log(ipfsHash);
       
@@ -56,41 +50,81 @@ describe('Stage test', function () {
       let registrationTimeout = datefns.addDays(currentTime,1);
      
       //combo 
-      const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-        modulusLength: 2048,
-      });
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+      //   modulusLength: 2048,
+      // });
       
-      await SC.setPublicKey(client, publicKey.toString());
+      // await SC.setPublicKey(client, publicKey.toString());
 
       // total deposit = beta+ lmin/li * beta + min reputation / current reputation  * beta
       let minimumDeposit = baseDeposit + minClientLevel/clientData[0] * baseDeposit + minimumReputation / 1 * baseDeposit;
 
-      client.deposit(minimumDeposit);
+      await SC.deposit(minimumDeposit, {from: client});
 
+      
       //prolong registration step?
 
     });
     it("commit-train stage", async function(){
       
-      const randomSecretKey  = crypto.generateKey('hmac', { length: 64 }, (err, key) => {
-        if (err) throw err;
-        console.log(key.export().toString('hex'));  // 46e..........620
-      });
-
-      const signature = crypto.sign('sha',randomSecretKey,privateKey);
-
-      const identity = ethCrypto.createIdentity();
-      const publicKey = ethCrypto.publicKeyByPrivateKey(
-        identity.privateKey
-      );
-      const privateKey = ethCrypto.publicKeyByPrivateKey(
-        identity.privateKey
-      );
+      const trainerIdentity = EthCrypto.createIdentity();
+      const modelOwnerIdentity = EthCrypto.createIdentity();
+      const randomSecretKey = EthCrypto.publicKeyByPrivateKey(modelOwnerIdentity.privateKey);
       
+      
+      
+      const hashedSecretKey  = EthCrypto.hash.keccak256(randomSecretKey);
+      const secretKeySignature = EthCrypto.sign(modelOwnerIdentity.privateKey, hashedSecretKey);
+      
+      const encryptedSecretKeyObject = await EthCrypto.encryptWithPublicKey(trainerIdentity.publicKey, hashedSecretKey);
+      const encryptedSecretKey = EthCrypto.cipher.stringify(encryptedSecretKeyObject);
+      console.log({modelOwnerIdentity})
+      console.log({trainerIdentity})
+      console.log({randomSecretKey});
+      console.log({hashedSecretKey});
+      console.log({encryptedSecretKey});
+      console.log({secretKeySignature})
+      
+      const parsedSecretKey = EthCrypto.cipher.parse(encryptedSecretKey);
+      const decryptedSecretKey = await EthCrypto.decryptWithPrivateKey(trainerIdentity.privateKey,parsedSecretKey)
 
+      const model = [];
+      // try {
 
+      //   let wrongDecryptSecretKey = await EthCrypto.decryptWithPrivateKey(modelOwnerIdentity.privateKey,parsedSecretKey)
+      //   console.log({wrongDecryptSecretKey});
+      // } catch (error) {
+      //   console.log({error});
+      // }
+      const recoveredSecretKeySignature = EthCrypto.recoverPublicKey(secretKeySignature,hashedSecretKey);
+      console.log({recoveredSecretKeySignature});
+      
+      console.log({decryptedSecretKey});
 
+      const ipfsData2 = {
+        secretKeySignature : secretKeySignature,
+        encryptedSecretKey : encryptedSecretKey
+      }
+      const secretKeyHash = EthCrypto.hash.keccak256(ipfsData2);
+      
+      // let c = await SC.getClient(client);
+      // console.log({clientKey},clientKey);
+      const hashedModel = EthCrypto.hash.keccak256(model);
+      const encryptedModelObject = await EthCrypto.encryptWithPublicKey(trainerIdentity.publicKey, hashedModel);
+      const encryptedModel = EthCrypto.cipher.stringify(encryptedModelObject);
+      const modelSignature = EthCrypto.sign(trainerIdentity.privateKey, hashedModel);
+      const ipfsData3 = {
+        modelSignature : modelSignature,
+        model : encryptedModel        
+      }
+      const commitHash = EthCrypto.hash.keccak256(ipfsData3);
 
-    })
+      console.log({secretKeyHash});
+      console.log({commitHash});
+
+      //reveal train stage
+      
+      
+    });
   });
 });
--- test/test-set-public-key.js
+++ test/test-set-public-key.js
@@ -31,27 +31,45 @@ describe('Stage test', function () {
     })
     
     it("set public key and signature", async function() {
-      
+
       const trainerIdentity = EthCrypto.createIdentity();
       const modelOwnerIdentity = EthCrypto.createIdentity();
       const randomSecretKey = EthCrypto.publicKeyByPrivateKey(modelOwnerIdentity.privateKey);
       
-      await SC.setPublicKey(client, trainerIdentity.publicKey);
-
-      let c = await SC.getClient(client);
-      // console.log({clientKey},clientKey);
       
-      let clientKey = c[4];
+      
       const hashedSecretKey  = EthCrypto.hash.keccak256(randomSecretKey);
       let signature = EthCrypto.sign(modelOwnerIdentity.privateKey, hashedSecretKey);
       
-      let encryptedSecretKey = EthCrypto.encryptWithPublicKey(clientKey, hashedSecretKey);
+      let encryptedSecretKeyObject = await EthCrypto.encryptWithPublicKey(trainerIdentity.publicKey, hashedSecretKey);
+      let encryptedSecretKey = EthCrypto.cipher.stringify(encryptedSecretKeyObject);
+      console.log({modelOwnerIdentity})
+      console.log({trainerIdentity})
+      console.log({randomSecretKey});
+      console.log({hashedSecretKey});
+      console.log({encryptedSecretKey});
+      console.log({signature})
+      
+      const parsedSecretKey = EthCrypto.cipher.parse(encryptedSecretKey);
+      let decryptedSecretKey = await EthCrypto.decryptWithPrivateKey(trainerIdentity.privateKey,parsedSecretKey)
+
+      try {
+
+        let wrongDecryptSecretKey = await EthCrypto.decryptWithPrivateKey(modelOwnerIdentity.privateKey,parsedSecretKey)
+        console.log({wrongDecryptSecretKey});
+      } catch (error) {
+        console.log({error});
+      }
+      let recoveredSignature = EthCrypto.recoverPublicKey(signature,hashedSecretKey);
+      console.log({recoveredSignature});
+      
+      console.log({decryptedSecretKey});
+
 
       
-      console.log({hashedSecretKey},hashedSecretKey);
-      console.log({encryptedSecretKey},encryptedSecretKey);
-      console.log({signature},signature);
-
+      // let c = await SC.getClient(client);
+      // console.log({clientKey},clientKey);
+      let commitHash = EthCrypto.hash.keccak256();
 
     })
 
