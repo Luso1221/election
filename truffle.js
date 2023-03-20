c84cbf26ad3eefe912433124f5b0ef39877f6252
Luso1221 <luso1221@gmail.com>
2023-03-20 11:05:21 +0900

    change truffle setting

--- truffle.js
+++ truffle.js
@@ -4,8 +4,18 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.8",
+    },
+  },
+  
+  mocha: {
+    timeout: 100000,
+    reporter: 'eth-gas-reporter'
+  },
 };
